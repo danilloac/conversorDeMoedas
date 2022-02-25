c07d4c9f88576448da68ca70359cb46fd6563abf
Danillo Spíndola <danilloac@hotmail.com>
2022-02-25 19:11:09 -0300

    conversor de moedas com React

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import ConversorMoedas from './conversor-moedas';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ConversorMoedas />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
